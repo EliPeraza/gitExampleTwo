c6faa573b937c650a95f82e37d24053311d98632
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-02-08 10:55:02 -0500

    first commit

---
+++ GitExampleTwo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitExampleTwo
+//
+//  Created by Elizabeth Peraza  on 2/8/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    print("WORKING ON GIT")
+    
+  }
+
+
+}
+
